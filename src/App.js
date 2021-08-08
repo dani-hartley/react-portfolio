ac439da1b03db0d72af1899c67c70567e30518f7
dhartley <dani_hartley@msn.com>
2021-08-08 17:09:53 -0500

    updated App.js and Nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react'; 
+import React, { useState } from 'react';
 import './App.css';
+import Nav from './components/Nav';
 import About from './components/About';
+import ContactForm from './components/Contact';
+
 
 function App() {
+  //Set state default to Home
+  const [currentPage, handlePageChange] = useState("About");
+  //Render page function to switch statement
+  const renderPage = () => {
+    switch (currentPage) {
+      case "About":
+        return <About />;
+      case "Contact":
+        return <ContactForm />
+    }
+  };
   return (
     <div>
       <main>
-        <About></About>
+        <Nav
+          currentPage={currentPage}
+          handlePagechange={handlePageChange}
+        />
+        <div>{renderPage(currentPage)}</div>
       </main>
     </div>
   );
---
+++ src/components/Nav/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Nav(props) {
+    const tabs = ['Home', 'About', 'Portfolio', 'Contact', 'Resume']
+
+    return (
+        <ul className="flex-row align-item-center nav-bar">
+            {tabs.map(tab => (
+                <li className={`nav-list ${props.currentPage === tab ? 'navActive' : 'nav-list'}`} key={tab}>
+                    <span data-testid={tab.toLowerCase()} onClick={() => props.handlePageChange(tab)}
+                        >
+                            {tab}
+                        </span>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default Nav;
