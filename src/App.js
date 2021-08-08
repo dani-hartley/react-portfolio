88f0dcdbf46b04fece9abcf8e6e9c313b8e1835e
dhartley <dani_hartley@msn.com>
2021-08-08 17:58:37 -0500

    added home section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Home from './components/Home';
 import About from './components/About';
 import ContactForm from './components/Contact';
 
 
 function App() {
   //Set state default to Home
-  const [currentPage, handlePageChange] = useState("About");
+  const [currentPage, handlePageChange] = useState("Home");
   //Render page function to switch statement
   const renderPage = () => {
     switch (currentPage) {
@@ -15,19 +16,23 @@ function App() {
         return <About />;
       case "Contact":
         return <ContactForm />
+      default: return <Home />;
     }
   };
   return (
     <div>
-      <main>
-        <Nav
-          currentPage={currentPage}
-          handlePagechange={handlePageChange}
-        />
-        <div>{renderPage(currentPage)}</div>
-      </main>
+        <main>
+            <Nav
+                currentPage={currentPage}
+                handlePageChange={handlePageChange}
+            />
+
+            <div>{renderPage(currentPage)}</div>
+
+            
+        </main>
     </div>
-  );
+);
 }
 
 export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,16 +4,23 @@ function Nav(props) {
     const tabs = ['Home', 'About', 'Portfolio', 'Contact', 'Resume']
 
     return (
-        <ul className="flex-row align-item-center nav-bar">
-            {tabs.map(tab => (
-                <li className={`nav-list ${props.currentPage === tab ? 'navActive' : 'nav-list'}`} key={tab}>
-                    <span data-testid={tab.toLowerCase()} onClick={() => props.handlePageChange(tab)}
-                        >
-                            {tab}
-                        </span>
-                </li>
-            ))}
-        </ul>
+        <header className="flex-row">
+            <h2>
+                <a href="https://dani-hartley.github.io/react-portfolio/">Dani Hartley</a>
+            </h2>
+            <nav className="flex-row">
+                <ul className="flex-row align-item-center nav-bar">
+                    {tabs.map(tab => (
+                        <li className={`nav-list ${props.currentPage === tab ? 'navActive' : 'nav-list'}`} key={tab}>
+                            <span data-testid={tab.toLowerCase()} onClick={() => props.handlePageChange(tab)}
+                            >
+                                {tab}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
     );
 }
 
